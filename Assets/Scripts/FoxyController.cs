22074fb68ceee10dae550e7ff281e27f3eff4341
Artyom <temkasafin@gmail.com>
2025-09-30 07:53:41 +0300

    Foxy now moves

---
+++ Assets/Scripts/FoxyController.cs
@@ -0,0 +1,114 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FoxyController : MonoBehaviour
+{
+    [SerializeField] private float motorForce = 100f;
+    [SerializeField] private float brakeForce = 1000f;
+    [SerializeField] private float maxSteerAngle = 15f;
+    [SerializeField] private float steerSpeed = 20f;
+
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+
+    [SerializeField] private WheelCollider frontLeftWheelColldier;
+    [SerializeField] private WheelCollider frontRighttWheelColldier;
+    [SerializeField] private WheelCollider rearLeftWheelColldier;
+    [SerializeField] private WheelCollider rearRighttWheelColldier;
+
+    private InputAction moveAction;
+    private InputAction brakeAction;
+
+    private float verticalInput;
+    private float horizontalInput;
+    private bool isBraking;
+
+    private float currentSteerAngle;
+    private float currentBrakingForce;
+
+    private float epsilon = 1e-3f;
+
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        brakeAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        getInput();
+        handleMotor();
+        handleSteering();
+        updateWheels();
+    }
+
+    private void getInput()
+    {
+        Vector2 moveResult = moveAction.ReadValue<Vector2>();
+        verticalInput = moveResult.y;
+        horizontalInput = moveResult.x;
+
+        isBraking = brakeAction.ReadValue<float>() > epsilon ? true : false;
+    }
+
+    private void handleMotor()
+    {
+        float newTorque = -verticalInput * motorForce;
+        frontLeftWheelColldier.motorTorque = frontRighttWheelColldier.motorTorque = newTorque;
+
+        currentBrakingForce = isBraking ? brakeForce : 0f;
+        applyBraking();
+    }
+
+    private void applyBraking()
+    {
+        frontLeftWheelColldier.brakeTorque = currentBrakingForce;
+        frontRighttWheelColldier.brakeTorque = currentBrakingForce;
+        rearLeftWheelColldier.brakeTorque = currentBrakingForce;
+        rearRighttWheelColldier.brakeTorque = currentBrakingForce;
+    }
+
+    private void handleSteering()
+    {
+        if (horizontalInput > 0)
+        {
+            currentSteerAngle = Mathf.Clamp(currentSteerAngle + steerSpeed * Time.deltaTime, -maxSteerAngle, maxSteerAngle);
+        }
+        else if (horizontalInput < 0)
+        {
+            currentSteerAngle = Mathf.Clamp(currentSteerAngle - steerSpeed * Time.deltaTime, -maxSteerAngle, maxSteerAngle);
+        }
+        else
+        {
+            if (currentSteerAngle > epsilon)
+            {
+                currentSteerAngle = Mathf.Clamp(currentSteerAngle - steerSpeed * Time.deltaTime, 0, maxSteerAngle);
+            }
+            else if (currentSteerAngle < epsilon)
+            {
+                currentSteerAngle = Mathf.Clamp(currentSteerAngle + steerSpeed * Time.deltaTime, -maxSteerAngle, 0);
+            }
+        }
+        frontLeftWheelColldier.steerAngle = frontRighttWheelColldier.steerAngle = currentSteerAngle;
+    }
+
+    private void updateWheelTransform(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+
+    private void updateWheels()
+    {
+        updateWheelTransform(frontLeftWheelColldier, frontLeftWheelTransform);
+        updateWheelTransform(frontRighttWheelColldier, frontRightWheelTransform);
+        updateWheelTransform(rearLeftWheelColldier, rearLeftWheelTransform);
+        updateWheelTransform(rearRighttWheelColldier, rearRightWheelTransform);
+    }
+}
